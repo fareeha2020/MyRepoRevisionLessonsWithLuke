7a25d0e6a22c9cfdb4a9ff4c8099100159ab3807
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-04 09:55:17 +1000

    revision 4/6/2020

--- switch.js
+++ switch.js
@@ -1,32 +1,46 @@
-// Q1 Create a switch statement that looks at the value of a number and gives you the book in the Harry Potter series that corresponds with that number. Series order is below
-let userInput=13;
-switch(userInput){
-    case 1:
-        console.log("1 - The Philosopher's Stone");
-        break;
-        case 2:
-            console.log("2 - The Chamber of Secrets");
-            break;
-            case 3:
-                console.log(" 3 - The Prisoner of Azkaban");
-                break;
-                case 4:
-                    console.log("4 - The Goblet of Fire");
-                    break;
-                    case 5:
-                        console.log(" 5 - The Order of the Phoenix");
-                        break;                       
-                        case 6:
-                        console.log(" 6 - The Half-Blood Prince");
-                        break;
-                        case 7:
-                        console.log(" 7 - The Deathly Hallowslet user");
-                        break;
-                        default:                        
-                                console.log("wrong selection");
-                                break;
+// // Q1 Create a switch statement that looks at the value of a number and gives you the book in the Harry Potter series that corresponds with that number. Series order is below
+// let userInput=13;
+// switch(userInput){
+//     case 1:
+//         console.log("1 - The Philosopher's Stone");
+//         break;
+//         case 2:
+//             console.log("2 - The Chamber of Secrets");
+//             break;
+//             case 3:
+//                 console.log(" 3 - The Prisoner of Azkaban");
+//                 break;
+//                 case 4:
+//                     console.log("4 - The Goblet of Fire");
+//                     break;
+//                     case 5:
+//                         console.log(" 5 - The Order of the Phoenix");
+//                         break;                       
+//                         case 6:
+//                         console.log(" 6 - The Half-Blood Prince");
+//                         break;
+//                         case 7:
+//                         console.log(" 7 - The Deathly Hallowslet user");
+//                         break;
+//                         default:                        
+//                                 console.log("wrong selection");
+//                                 break;
         
-}
- 
+// }
 
-// Q2 Convert the switch statement above into an if..else if..else statement
+ let inputDay="Saturday";
+ switch(inputDay){
+     case "Thursday":
+     case "Friday":
+         console.log("Thursday/Friday - Yay, it's almost the weekend");       
+         case "Saturday":
+            console.log("Saturday - Ahh, I can finally relax");
+            break;
+            case "Sunday":
+                console.log("Sunday - Ah well, the weekend is nearly over");
+                break;
+                default:
+                    console.log("default - Time for work then...");
+                    break;
+ }
+
--- switchtoIFelse.js
+++ switchtoIFelse.js
@@ -1,30 +1,42 @@
 // Q2 Convert the switch statement above into an if..else if..else statement
-let userInput=13;
-if(userInput==1){
-console.log("1 - The Philosopher's Stone");
+// let userInput = 13;
+// if (userInput == 1) {
+//     console.log("1 - The Philosopher's Stone");
+// }
+// else if (userInput == 2) {
+//     console.log("2 - The Chamber of Secrets");
+// }
+// else if (userInput == 3) {
+//     console.log("3 - The Prisoner of Azkaban");
+// }
+// else if (userInput == 4) {
+//     console.log("4 - The Goblet of Fire");
+// }
+// else if (userInput == 5) {
+//     console.log("5 - The Order of the Phoenix");
+// }
+// else if (userInput == 6) {
+//     console.log("6 - The Half-Blood Prince");
+// }
+// else if (userInput == 7) {
+//     console.log("7 - The Deathly Hallows");
+// }
+// else {
+//     console.log("wrong selection");
+// }
+// Q2 Convert the switch statement  into an if..else if..else statement
+let inputDay = "Saturday";
+if (inputDay == "Thursday" || inputDay == "Friday") {
+    console.log("Thursday/Friday - Yay, it's almost the weekend");
 }
-else if(userInput==2){
-    console.log("2 - The Chamber of Secrets");
+else if (inputDay == "Saturday") {
+    console.log("Saturday - Ahh, I can finally relax");
 }
-else if(userInput==3){
-    console.log("3 - The Prisoner of Azkaban");
+else if (inputDay == "Sunday") { console.log("Sunday - Ah well, the weekend is nearly over"); }
+else {
+    console.log("default - Time for work then...");
 }
-else if(userInput==4){
-    console.log("4 - The Goblet of Fire");
-}
-else if(userInput==5){
-    console.log("5 - The Order of the Phoenix");
-}
-else if(userInput==6){
-    console.log("6 - The Half-Blood Prince");
-}
-else if(userInput==7){
-    console.log("7 - The Deathly Hallows");
-}
-else
-{
-    console.log("wrong selection");
-}
+
 
 
 
