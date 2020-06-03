029194968d7ef21d8c7d76e6bc8db0b7c84da7e0
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-03 12:25:46 +1000

    revision

--- LogicalopExercice.js
+++ LogicalopExercice.js
@@ -4,13 +4,37 @@
 // }else{
 //     console.log("can drink");
 // }
-let opentime = 17;
-let closetime = 22;
-let currenttime = 18;
-let isMonday = "Mnday";
-if (currenttime > closetime || currenttime < opentime || isMonday == "Monday") {
-    console.log(" restaurant is closed");
+//EXERCISE2
+// let opentime = 17;
+// let closetime = 22;
+// let currenttime = 18;
+// let isMonday = true;
+// if (currenttime > closetime || currenttime < opentime || isMonday) {
+//     console.log(" restaurant is closed");
+// }
+// else {
+//     console.log("yes restaurant is open");
+// }
+//Exercise 3
+// let food1="";
+// let food2="";
+// let food3;//undefined is falsy
+// let popularFood=food1||food2||food3||"Pizza"
+// console.log(popularFood);
+//EXERCISE 4 AND STATEMENT
+// let opentime = 17;
+// let closetime = 22;
+// let currenttime =20;
+// //let isMonday = true;
+// if (currenttime < closetime &&currenttime > opentime ) {
+//     console.log(" restaurant is open");
+// }
+// else {
+//     console.log("restaurant is closed");
+// }
+//Exercise 5 NOT STATEMENT
+let favRest;
+if(!favRest){
+   favRest="Jasmine"
 }
-else {
-    console.log("yes restaurant is open");
-}
+console.log(favRest);
