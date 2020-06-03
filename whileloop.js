9958713ded52999e3f6b1823cebe8a4144aa626f
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-03 14:00:28 +1000

    revision

---
+++ whileloop.js
@@ -0,0 +1,33 @@
+
+console.log(" printing from 3 to 8");
+let i = 3;
+while (i <= 8) {
+    console.log(i);
+    i++;
+}
+
+console.log(" divided by 2 between 14 and 20");
+let j = 14;
+while (j <= 20) {
+    if (j % 2 == 0) {
+        console.log(j);
+    }
+    j++;
+}
+
+console.log("rev printing 12 to 7");
+let k = 12;
+while (k >= 7) {
+    console.log(k);
+    k--;
+
+}
+
+console.log("Divided by 3 between 33 and 45 in reverse order");
+let l = 45;
+while (l >= 33) {
+    if (l % 3 == 0) {
+        console.log(l);
+    }
+    l--;
+}
