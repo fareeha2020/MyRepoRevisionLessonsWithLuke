f2a5351c5c52665c07e936553345889b078b1418
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-04 09:43:03 +1000

    revision

---
+++ switchtoIFelse.js
@@ -0,0 +1,35 @@
+// Q2 Convert the switch statement above into an if..else if..else statement
+let userInput=13;
+if(userInput==1){
+console.log("1 - The Philosopher's Stone");
+}
+else if(userInput==2){
+    console.log("2 - The Chamber of Secrets");
+}
+else if(userInput==3){
+    console.log("3 - The Prisoner of Azkaban");
+}
+else if(userInput==4){
+    console.log("4 - The Goblet of Fire");
+}
+else if(userInput==5){
+    console.log("5 - The Order of the Phoenix");
+}
+else if(userInput==6){
+    console.log("6 - The Half-Blood Prince");
+}
+else if(userInput==7){
+    console.log("7 - The Deathly Hallows");
+}
+else
+{
+    console.log("wrong selection");
+}
+
+
+
+
+
+
+
+
