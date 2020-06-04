768113c7123ddb41ea752977aecf00aaf8d8f314
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-05 09:26:06 +1000

    daily challeng 4/6/2020

--- ArrayExercise.js
+++ ArrayExercise.js
@@ -3,20 +3,20 @@ array1.push('a');
 array1.push('b');
 array1.push('c');
 console.log("reverse order printing")
-for (let i = array1.length; i >= 0; i--) {
+for (let i = array1.length-1; i >= 0; i--) {
     console.log(array1[i]);
 
 }
 let array2 = new Array(1, 12, 3, 14);
+array2.unshift(10);
 array2.push(15);
 console.log("even index printing");
 for (let i = 0; i < array2.length; i++) {
     if (i % 2 == 0) {
         console.log(`${array2[i]} at ${i}`);
     }
-
 }
-console.log("even value printing using for of")
+console.log("even value printing using :for of")
 for (let eachElement of array2) {
     if (eachElement % 2 == 0) {
         console.log(eachElement);
