68272cb562e7e56d1bbf8954984040055e4d612b
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-04 11:49:49 +1000

    yes

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
-function sayHelloInside(name) {
+function sayHelloInside(name="Fareeha") {
     console.log(`Hello ${name}`);
 }
-function returnHelloStmt(name) {
+function returnHelloStmt(name="Luke") {
     return `Hi ${name}`;
 
 }
-function logLargest(num1, num2) {
+function logLargest(num1, num2=100) {
     if (num1 > num2) {
         console.log(`the largest of ${num1} &${num2} is ${num1}`);
     }
@@ -14,7 +14,7 @@ function logLargest(num1, num2) {
     }
 }
 
-function returnSmall(num1, num2) {
+function returnSmall(num1=5, num2=5) {
     if (num1 < num2) {
         return `the smallest of ${num1} &${num2} is ${num1}`;
     }
@@ -22,7 +22,13 @@ function returnSmall(num1, num2) {
         return `the smallest of ${num1}& ${num2} is ${num2}`;
     }
 }
-sayHelloInside("fareeha");
+sayHelloInside("Zoya");
 console.log(returnHelloStmt("Tom"));
 logLargest(11, 10);
 console.log(returnSmall(22, 14));
+console.log("........demonstrating Default Value..........");
+sayHelloInside();
+console.log(returnHelloStmt());
+logLargest(11);
+console.log(returnSmall());
+console.log(returnSmall(2));
