079eb96c0884cb93fb60e50afe3fca3cc3f85fb4
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-04 10:52:02 +1000

    rev

---
+++ function.js
@@ -0,0 +1,28 @@
+function sayHelloInside(name) {
+    console.log(`hello ${name}`);
+}
+function returnHelloStmt(name) {
+    return `hello ${name}`;
+
+}
+function logLargest(num1, num2) {
+    if (num1 > num2) {
+        console.log(`the largest of ${num1} &${num2} is ${num1}`);
+    }
+    else {
+        console.log(`the largest of ${num1} &${num2} is ${num2}`);
+    }
+}
+
+function returnSmall(num1, num2) {
+    if (num1 < num2) {
+        return `the smallest of ${num1} &${num2} is ${num1}`;
+    }
+    else {
+        return `the smallest of ${num1}& ${num2} is ${num2}`;
+    }
+}
+sayHelloInside("fareeha");
+console.log(returnHelloStmt("Tom"));
+logLargest(11, 10);
+console.log(returnSmall(22, 14));
