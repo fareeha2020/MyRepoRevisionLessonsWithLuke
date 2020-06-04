5abe134f4adf56920e3381a30b53c7903125070d
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-04 13:35:06 +1000

    revision

--- functiontoarrow.js
+++ functiontoarrow.js
@@ -1,8 +1,5 @@
 const sayHelloInside=(name)=> console.log(`Hello ${name}`);
-
 const returnHelloStmt=(name)=> `Hi ${name}`;
-
-
 const logLargest=(num1, num2)=>{
     if (num1 > num2) {
         console.log(`the largest of ${num1} &${num2} is ${num1}`);
@@ -11,7 +8,6 @@ const logLargest=(num1, num2)=>{
         console.log(`the largest of ${num1} &${num2} is ${num2}`);
     }
 }
-
 const returnSmall=(num1, num2) =>{
     if (num1 < num2) {
         return `the smallest of ${num1} &${num2} is ${num1}`;
@@ -20,7 +16,7 @@ const returnSmall=(num1, num2) =>{
         return `the smallest of ${num1}& ${num2} is ${num2}`;
     }
 }
-sayHelloInside("Zoya");
+sayHelloInside("Reeha");
 console.log(returnHelloStmt("Tom"));
 logLargest(11, 10);
 console.log(returnSmall(22, 14));
