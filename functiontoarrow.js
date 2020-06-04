ace82b764c0e2ab0d6ec1a3b8ca1d05d91351a82
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-04 12:33:42 +1000

    commit

--- functiontoarrow.js
+++ functiontoarrow.js
@@ -1,11 +1,9 @@
-function sayHelloInside(name="Fareeha") {
-    console.log(`Hello ${name}`);
-}
-function returnHelloStmt(name="Luke") {
-    return `Hi ${name}`;
+const sayHelloInside=(name)=> console.log(`Hello ${name}`);
 
-}
-function logLargest(num1, num2=100) {
+const returnHelloStmt=(name)=> `Hi ${name}`;
+
+
+const logLargest=(num1, num2)=>{
     if (num1 > num2) {
         console.log(`the largest of ${num1} &${num2} is ${num1}`);
     }
@@ -14,7 +12,7 @@ function logLargest(num1, num2=100) {
     }
 }
 
-function returnSmall(num1=5, num2=5) {
+const returnSmall=(num1, num2) =>{
     if (num1 < num2) {
         return `the smallest of ${num1} &${num2} is ${num1}`;
     }
@@ -26,12 +24,7 @@ sayHelloInside("Zoya");
 console.log(returnHelloStmt("Tom"));
 logLargest(11, 10);
 console.log(returnSmall(22, 14));
-console.log("........demonstrating Default Value..........");
-sayHelloInside();
-console.log(returnHelloStmt());
-logLargest(11);
-console.log(returnSmall());
-console.log(returnSmall(2));
+
 
 
 
